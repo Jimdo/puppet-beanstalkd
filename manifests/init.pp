bef44071a27055ff03e481ee71e5da7cd9696f04
zined <zined@adrianer.de>
2013-04-22 13:58:34 +0200

    initial beanstalk module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,66 @@
-# Install/configure something here
-class skeleton {
-}
+################################################################################
+# Class: beanstalkd
+#
+# This module manages the beanstalkd service.
+#
+# Parameters:
+#
+#  $listen_addr:: Listen on address <addr> (default is 127.0.0.1)
+#  $listen_port:: Listen on TCP port <port> (default is 11300).
+#  $start_service:: true/false. (default is false)
+#  $binlog_dir:: Use a binlog to keep jobs on persistent storage in <dir>.
+#                Upon startup, beanstalkd will recover any binlog that is
+#                present in <dir>, then, during normal operation, append new
+#                jobs and changes in state to the binlog.
+#  $binlog_max_size:: The maximum size in bytes of each binlog file.
+#  $fsync_max:: Call fsync(2) at most once every <ms> milliseconds.
+#               This will recuce disk activity and improve speed at the
+#               cost of safety. A power failure could result in the loss of
+#               up to <ms> milliseconds of history. A <ms> value of 0 will
+#               cause beanstalkd to call fsync every time it writes to
+#               the binlog.
+#  $job_max_size:: The maximum size in bytes of a job.
+#
+# Sample Usage:
+#
+#  class { 'beanstalkd':
+#    listen_addr => '1.2.3.4',
+#    listen_port => 12345,
+#    binlog_dir => '/var/lib/beanstalkd/binlog'
+#  }
+#
+class beanstalkd(
+  $listen_addr      = '127.0.0.1',
+  $listen_port      = '11300',
+  $start_service    = false,
+  $binlog_dir       = undef,
+  $binlog_max_size  = undef,
+  $fsync_max        = undef,
+  $job_max_size     = undef
+) {
 
-class { 'skeleton': }
+  case $::operatingsystem {
+    debian, default: {
+      $config_file      = '/etc/default/beanstalkd'
+      $config_template  = 'config.debian.erb'
+    }
+  }
+
+  package { 'beanstalkd':
+    ensure => present,
+  }
+
+  ->file { 'beanstalkd_config':
+    ensure    => present,
+    path      => $config_file,
+    content   => template("beanstalkd/${config_template}"),
+    notify    => Service[beanstalkd]
+  }
+
+  ->service { 'beanstalkd':
+    ensure    => $start_service ? {
+      false => 'stopped',
+      true  => 'running'
+    }
+  }
+}
